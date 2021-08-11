c232f99a06935f829a39afaa0b28a94f0a750957
guestSoftMe <73720335+guestSoftMe@users.noreply.github.com>
2021-08-11 18:01:57 +0300

    todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from "react";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import Todo from "./todo";
+import {Context} from "./context";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let App = () => {
+    const [state, setState] = useState([])
+    const [title, setTitle] = useState('');
+
+    useEffect(()=>{
+        let local = localStorage.getItem('todo')
+        setState(JSON.parse(local))
+    },[])
+
+    useEffect(()=>{
+        localStorage.setItem('todo',JSON.stringify(state))
+    },[state])
+
+    let btnadd = () => {
+        if (title === '') {
+          return false
+        }else{
+            setState([
+                ...state,
+                {
+                    id: Date.now(),
+                    todo: title,
+                    done: true
+                }
+            ])
+            setTitle('')
+        }
+    }
+    const delet = id =>{
+        setState(state.filter(i=>{
+            return i.id !== id
+        }))
+    }
+
+    return (
+        <Context.Provider value={{delet}}>
+        <div className='App'>
+            <div>
+                <div><TextField
+                    id="standard-full-width"
+                    label="TODO"
+                    style={{margin: 8, width: '500px'}}
+                    placeholder="Todo name"
+                    margin="normal"
+                    InputLabelProps={{
+                        shrink: true,
+                    }}
+                    value={title}
+                    onChange={event => setTitle(event.target.value)}
+                /></div>
+                <Button variant="contained" color="primary" onClick={btnadd}>
+                    Добавить
+                </Button>
+                <Todo todos={state}/>
+            </div>
+        </div>
+    </Context.Provider>
+    )
 }
 
 export default App;
